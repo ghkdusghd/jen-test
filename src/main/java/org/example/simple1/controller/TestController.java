951b80bfc14f8b0c04b25c509a7a8f1fa86134ed
ghkdusghd <164862216+ghkdusghd@users.noreply.github.com>
2024-07-18 21:51:05 +0900

    test commit

--- src/main/java/org/example/simple1/controller/TestController.java
+++ src/main/java/org/example/simple1/controller/TestController.java
@@ -11,8 +11,8 @@ public class TestController {
     @GetMapping("/")
     public MemberDto test() {
         MemberDto memberDto = new MemberDto();
-        memberDto.setName("fucking jenkins");
-        memberDto.setAge(20);
+        memberDto.setName("pumpkin");
+        memberDto.setAge(30);
         return memberDto;
     }
 }
